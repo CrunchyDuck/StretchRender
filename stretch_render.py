789521a70658e5e44fa195da8eac3b9fce20a729
Duck <43281440+CrunchyDuck@users.noreply.github.com>
2020-04-14 03:38:06 +0100

    Concept Draft
    The first draft with rough ideas on the structure of the code required.

--- stretch_render.py
+++ stretch_render.py
@@ -27,12 +27,19 @@ class RENDER_OT_stretch_render(bpy.types.Operator):
 	# This code is written while tired, busy, and unfocused. It is just to lay out an idea on what I should do when I return.
 	def execute(self, context):
 		target_scene = ""
-		active_items_in_scene = ""
+		active_items_in_scene = "" # This should collect all THINGS with a key frame on them. I don't know how I will check for keyframes for things such as render settings, however.
+			# I might be able to use the list of "actions"?
+		animation_frames = 0
 
+		for i in range(animation_frames):
+			if animation_on_this_frame:
+				pass
+				# Check each animation to make sure it is set to CONSTANT interpolation. If it is, we can save a "stretch" of this frame.
+			else:
+				pass
+				# If any are found to not be constant, we will have to check what the item with the longest interpolation is, and then simply "skip" ahead that amount as we can't cut anything out in that case.
+				# Since I'm going to be creating a new .blend file to store this new version of the animation in, I will still need to "save" all of the animations stored between the two points.
 
-		for i in range(len(active_items_in_scene)):
-			current_object = active_items_in_scene[i]
-			object_keyframes = active_items_in_scene.keyframes
 
 
 		return {"FINISHED"}
